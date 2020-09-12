4aebf8d47e17bc9299cdbd25f5da707333771e18
krehwell <fahrimuham001@gmail.com>
2020-09-12 21:32:25 +0800

    able to send messages one to each other | not able to detect who calls automatically still

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 // ----- " initiate new user " -----
-const peer = new Peer();
-var _id;
+var _id = "fuck" + Math.floor(Math.random() * 10);
+const peer = new Peer(_id);
 
 // ----- " Init Peer " -----
 peer.on("open", (id) => {
   _id = id;
-  document.getElementById("peerid").value = id;
-  console.log(id);
-
-  peer.on("data", (data) => {
-    console.log("Data received: " + data);
-  });
-
-  // peer.send("fuck you");
+  document.getElementById("peerid").value = _id;
+  console.log("peer id: " + id);
 });
 
 // ----- " Receiver " -----
 // when the connection is enstablihed
-peer.on("connection", (conn) => {
+peer.on("connection", (connected) => {
   console.log("the user are calling");
-  conn.send("fuck")
+  // received the data sent by the sender
+  connected.on("data", data => console.log(data))
 });
 
-peer.on("error", (error)=> {
-  console.log(error)
-})
+peer.on("error", (error) => {
+  console.log(error);
+});
 
 // ----- " Sender " -----
+var conn; // init the conn
 function onClickBtnCall() {
+  // call the end user
   let callerId = document.getElementById("peertocall").value;
-  var conn = peer.connect(callerId);
+  conn = peer.connect(callerId);
+}
+
+// send fuck you to end
+function sendFuck() {
+  console.log(conn);
+  conn.send({peer: conn.peer, data: "fuck you", to: conn.provider._id})
 }
