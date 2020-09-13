8441e0f21290f480cb7e83bffc628a2c4da3f3b4
krehwell <fahrimuham001@gmail.com>
2020-09-14 01:40:40 +0800

    adding sweet alert and changing binding navigator getMedia

--- app.js
+++ app.js
@@ -1,38 +1,28 @@
-// DEFINING THE GETUSERMEDIA --- SRC: https://github.com/twinysam/VoiceApp/blob/master/index.html
-var getUserMedia = (function () {
-	if (navigator.getUserMedia) {
-		return navigator.getUserMedia.bind(navigator)
-	}
-	if (navigator.webkitGetUserMedia) {
-	 return navigator.webkitGetUserMedia.bind(navigator)
-	}
-	if (navigator.mozGetUserMedia) {
-		return navigator.mozGetUserMedia.bind(navigator)
-	}
-})();
+// DEFINING THE getUserMedia()
+var getUserMedia = navigator.getUserMedia
 // END OF DIFINING GETUSERMEDIA
 
 // ----- " INITIATE NEW USER " -----
-var _id = "fuck" + Math.floor(Math.random() * 1000);                                  // user id to init
-var conn;                                                                             // the guy who will received
-var nobodyconnected = true;                                                           // make sure that nobody is connected to user
-const peer = new Peer(_id);                                                           // init the peer(user) to generate id
+var _id = "fuck" + Math.floor(Math.random() * 1000);                            // user id to init
+var conn;                                                                       // the guy who will received
+var nobodyconnected = true;                                                     // make sure that nobody is connected to user
+const peer = new Peer(_id);                                                     // init the peer(user) to generate id
 
 // ----- " INIT PEER " -----
 peer.on("open", (id) => {
-  _id = id;                                                                           // let the id is fetched by the server
-  $("#peerid").val(_id)                                                               // put the "id" to the DOM 
+  _id = id;                                                                     // let the id is fetched by the server
+  $("#peerid").val(_id)                                                         // put the "id" to the DOM 
   console.log("peer id: " + id);
 });
 
 // ----- " RECEIVER " -----
 // when the data connection is enstablished
 peer.on("connection", (connected) => {
-  let caller = connected.peer;                                                        // the guy who received take the info of caller(user) 
+  let caller = connected.peer;                                                  // the guy who received take the info of caller(user) 
   console.log(caller + " are data connected");
-  $('#info').text("you are connected to " + caller);                                  // set the guy who received DOM who is calling
+  $('#info').text("you are connected to " + caller);                            // set the guy who received DOM who is calling
 
-  if(nobodyconnected){                                                                // let the user know that the guy who received has been connected
+  if(nobodyconnected){                                                          // let the user know that the guy who received has been connected
     (async () => { try{ conn = await peer.connect(caller); nobodyconnected = false;}  // IIFE with Async
      catch(err){console.log(err)} })();
   }
@@ -46,44 +36,52 @@ peer.on("connection", (connected) => {
 });
 
 // in order to peer to received the call and passback the media to the caller
-peer.on("call", call => {
-  if(!confirm(`${call.peer} is calling, Answer?`)){return ;}                          // allow or reject the call received
-
-  getUserMedia({audio: true, video: false}, (media) => {                              // passing peer media to the caller
-    console.log("you hit someone else to pass your media");
-    call.answer(media);
-  })
+peer.on("call", async call => {
+  // pick up the call or not
+  Swal.fire({
+    title: `${call.peer} is calling you`,
+    showDenyButton: true, confirmButtonText: `Pick Up`, denyButtonText: `Decline`,
+    customClass: { confirmButton: "order-1", denyButton: "order-3", },
+  }).then((result) => {
+    if (result.isConfirmed) {                                                   // if pick up then get the media then send and bind to DOM
+      getUserMedia({ audio: true, video: false }, (media) => {
+        console.log("you hit someone else to pass your media");                 // passing peer media to the caller
+        call.answer(media);                                                     // answer to trigger
+      });
+    
+      call.on("stream", (media) => {
+        // passing the caller media to DOM
+        console.log("you are stream now");
+        let mediasource = document.getElementById("audiostream");
+        mediasource.srcObject = media;
+        mediasource.play();
+      });
+    }
+    else{ return; }
+  });
 
   console.log(`${call.peer} is calling to you`)
-
-  call.on('stream', (media)=>{                                                        // passing the caller media to DOM
-    console.log("you are stream now")
-    let mediasource = document.getElementById("audiostream");
-    mediasource.srcObject = media;
-    mediasource.play();
-  })
 })
 
 // any connection error debugger
 peer.on("error", (error) => {                                                         
-  $("#infoerror").html(`Error Occur - Thinking to reload the browser maybe?`)
+  $("#infoerror").html(`Error Occur(<b>${error.type}</b>) - Thinking to reload the browser maybe?`)
   // clear error message in 10 sec
-  setTimeout(()=>{
-    $("#infoerror").html(``);
-  }, 10000);
-  console.log(`${error.type} - ${error}`);
+  setTimeout(()=>{ $("#infoerror").html(``); }, 10000); console.log(`${error.type} - ${error}`);
 });
 
 // ----- " SENDER " -----
-function onErrorMediaCallback(){                                                      // error getUserMedia() handler
+function onErrorMediaCallback(){
   console.log("error on navigator.getUserMedia()");
+  $("#infoerror").html(`Please allow you browser to use your mic, otherwise how are you gonna communicate asshole`)
+  setTimeout(()=>{ $("#infoerror").html(``); }, 8000);                          // clear error message in 8 sec
 }
 
 // calling the guy
-function onClickBtnCall() {                                                           // user calling the id of the guy
+function onClickBtnCall() {                                                     // user calling the id of the guy
   // call the end user
-  let callerId = $("#peertocall").val();                                              // get the id input form the DOM 
-  conn = peer.connect(callerId);                                                      // try to connect data
+  let callerId = $("#peertocall").val();                                        // get the id input form the DOM 
+  conn = peer.connect(callerId);                                                // try to connect data
 
   // ---- " calling using audio " -----
   getUserMedia({video: false,audio: true}, function(stream){
@@ -106,7 +104,7 @@ function onClickBtnCall() {                                                     
 // send fuck you to end
 function sendFuck() {
   let data = {from: conn.provider._id , message: "fuck you", to: conn.peer}
-  conn.send(data)                                                                      // send fuck to end user
+  conn.send(data)                                                              // send fuck to end user
   let messagebuffer = $('#message-sent');
   messagebuffer.append(`<p>${data.from}: ${data.message}</p>`)
 }
